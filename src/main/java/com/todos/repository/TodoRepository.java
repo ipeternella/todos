e8a84c8f1bc71d965b2b24c30e48ca4b1da9a4a4
IgooorGP <igor.feq@gmail.com>
2017-12-20 12:10:50 -0200

    Added lombok dependency and refactored Todo class to dry the code.

--- src/main/java/com/todos/repository/TodoRepository.java
+++ src/main/java/com/todos/repository/TodoRepository.java
@@ -1,15 +1,11 @@
 package com.todos.repository;
 
-import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import com.todos.domain.Todo;
 
 @Repository
 public interface TodoRepository extends MongoRepository<Todo, String> {
-	
-	public List<Todo> findByUser(String user);
-	
+		
 }
