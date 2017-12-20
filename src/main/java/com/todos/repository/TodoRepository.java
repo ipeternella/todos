da77b27942b9e0bd8d210b4cda89074775e82cd9
IgooorGP <igor.feq@gmail.com>
2017-12-20 20:58:05 -0200

    Added FixtureFactory for easy creation of fake objects.Passed tests for usecase layer.

--- src/main/java/com/todos/repository/TodoRepository.java
+++ src/main/java/com/todos/repository/TodoRepository.java
@@ -1,11 +1,10 @@
 package com.todos.repository;
 
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.todos.domain.Todo;
 
 @Repository
 public interface TodoRepository extends MongoRepository<Todo, String> {
-		
+	
 }
--- src/test/groovy/com/todos/usecases/CrudTodoSpec.groovy
+++ src/test/groovy/com/todos/usecases/CrudTodoSpec.groovy
@@ -4,19 +4,22 @@ import spock.lang.Specification
 import com.todos.domain.Todo
 import com.todos.services.TodoService
 import com.todos.repository.TodoRepository
+import com.todos.usecase.CrudTodo
+import br.com.six2six.fixturefactory.Fixture
+import br.com.six2six.fixturefactory.Rule
 
 class CrudTodoSpec extends Specification {
-
-	TodoRepository todoRepo = Mock(TodoRepository)
-	TodoService todoService = new TodoService("todoRepo": todoRepo)
 		
-	def "Should INSERT a new todo in the database by invoking TodoService.create"() {
-		when: "I call TodoService.create()"		
+	def "Test CrudTodo class" () {
+	    TodoService todoService = Mock(TodoService)
+		CrudTodo crudTodo = new CrudTodo(todoService: todoService)
+	
+		when: "I call crudTodo.create()"		
 			Todo dummyTodo = getDummyTodo()
-			Todo createdTodo = todoService.create(dummyTodo)
+			Todo createdTodo = crudTodo.create(dummyTodo)
 							
-		then: "Should invoke Repository save()"
-			1 * todoRepo.save(_) >> dummyTodo // fakes a Todo return to continue testing todoService.create() 
+		then: "Should invoke service.create()"
+			1 * todoService.create(_) >> dummyTodo // crudTodo.create() should invoke service.create() 
 			
 		and: "Should match the dummy Todo"
 			assert createdTodo != null
@@ -24,11 +27,28 @@ class CrudTodoSpec extends Specification {
 			assert createdTodo.getUser() == "someone u dunno"
 			assert createdTodo.getTask() == "walk the dog, dude!"
 			assert createdTodo.isCompleted() == false		 
-	} 	
+	}
 	
-	def getDummyTodo() {
-		Todo dummyTodo = new Todo("000", "someone u dunno", "walk the dog, dude!", false)
-		
-		return dummyTodo		
+	def "Test TodoService class" () {
+		TodoRepository todoRepo = Mock(TodoRepository)
+		TodoService todoService = new TodoService(todoRepo: todoRepo)
+				
+		when: "I call todoService.create()"
+			Todo dummyTOdo = getDummyTodo() 
+			todoService.create(dummyTodo)
+			
+		then: "should invoke MongoRepository.save()"
+			1 * todoRepo.save(_)  
+	}
+			
+	def getDummyTodo() {		
+		Fixture.of(Todo.class).addTemplate("test", new Rule() {{
+			add("id", "000"); 
+			add("user", "someone u dunno");
+			add("task", "walk the dog, dude!");
+			add("completed", false);
+		}});
+				
+		return Fixture.from(Todo.class).gimme("test")		
 	}
 }
