4c3cc52b61244f70505c5e0fb4c6eb7bbb25f45d
IgooorGP <igor.feq@gmail.com>
2017-12-25 15:02:55 -0200

    Removed hard tabs and indented pom.xml properly

--- src/test/groovy/com/todos/test/services/TodoServiceSpec.groovy
+++ src/test/groovy/com/todos/test/services/TodoServiceSpec.groovy
@@ -3,11 +3,11 @@ package com.todos.test.services
 import spock.lang.Specification
 
 class TodoServiceSpec extends Specification {
-	
-	def "hehehe"() {
-		def x = 1
-		
-		assert x == 2
-	}
-	
+    
+    def "hehehe"() {
+        def x = 1
+        
+        assert x == 2
+    }
+    
 }
--- src/test/groovy/com/todos/test/usecases/TodoCRUDSpec.groovy
+++ src/test/groovy/com/todos/test/usecases/TodoCRUDSpec.groovy
@@ -18,66 +18,66 @@ import br.com.six2six.fixturefactory.Rule
 
 class TodoCRUDSpec extends Specification {
 
-	// fields
-	TodoService todoService = Mock(TodoService) // mocks service that calls the repository layer
-	TodoCRUD todoCRUD = new TodoCRUD(todoService: todoService)
+    // fields
+    TodoService todoService = Mock(TodoService) // mocks service that calls the repository layer
+    TodoCRUD todoCRUD = new TodoCRUD(todoService: todoService)
 
-	// feature methods
-	
-	/**
-	* CREATE CRUD operation
-	*/
-	def "creating a todo" () {
-		when: "controller calls todoCRUD.create method"		
-			// dummy todo for database insert							
-			Todo dummyTodo = getDummyTodo() 
-			
-			Todo createdTodo = todoCRUD.create(dummyTodo)
-							
-		then: "should invoke service.create()"		
-			1 * todoService.create(_) >> dummyTodo 
-			
-		and: "service's result should match the createdTodo"
-			matchesDummyTodo(createdTodo)
-	}
-		
-	/** 
-	* READ operation
-	*/ 
-	def "getting one todo by its mongo id" () {	
-		when: "controller calls todoCRUD.findById method"
-			// fake id
-			String todoId = "1"									
-			Todo foundTodo = todoCRUD.findById(todoId)
-							
-		then: "should invoke service.findById()"			
-			1 * todoService.findById(_) >> getDummyTodo()
-			
-		and: "service's result should match the foundTodo"
-			matchesDummyTodo(foundTodo)
-		
-	}
-		
-	// helper methods
-		
-	// helper function to create a dummy todo		
-	def getDummyTodo() {		
-		Fixture.of(Todo.class).addTemplate("test", new Rule() {{
-			add("id", "101"); 
-			add("user", "someone");
-			add("task", "walk the dog");
-			add("completed", false);
-		}});
-				
-		return Fixture.from(Todo.class).gimme("test")		
-	}
-	
-	// helper function to match a dummy todo
-	void matchesDummyTodo(todo) {
-		assert todo.getId() == "101"
-		assert todo.getUser() == "someone"
-		assert todo.getTask() == "walk the dog"
-		assert todo.isCompleted() == false
-	}
-	
+    // feature methods
+    
+    /**
+    * CREATE CRUD operation
+    */
+    def "creating a todo" () {
+        when: "controller calls todoCRUD.create method"        
+            // dummy todo for database insert                            
+            Todo dummyTodo = getDummyTodo() 
+            
+            Todo createdTodo = todoCRUD.create(dummyTodo)
+                            
+        then: "should invoke service.create()"        
+            1 * todoService.create(_) >> dummyTodo 
+            
+        and: "service's result should match the createdTodo"
+            matchesDummyTodo(createdTodo)
+    }
+        
+    /** 
+    * READ operation
+    */ 
+    def "getting one todo by its mongo id" () {    
+        when: "controller calls todoCRUD.findById method"
+            // fake id
+            String todoId = "1"                                    
+            Todo foundTodo = todoCRUD.findById(todoId)
+                            
+        then: "should invoke service.findById()"            
+            1 * todoService.findById(_) >> getDummyTodo()
+            
+        and: "service's result should match the foundTodo"
+            matchesDummyTodo(foundTodo)
+        
+    }
+        
+    // helper methods
+        
+    // helper function to create a dummy todo        
+    def getDummyTodo() {        
+        Fixture.of(Todo.class).addTemplate("test", new Rule() {{
+            add("id", "101"); 
+            add("user", "someone");
+            add("task", "walk the dog");
+            add("completed", false);
+        }});
+                
+        return Fixture.from(Todo.class).gimme("test")        
+    }
+    
+    // helper function to match a dummy todo
+    void matchesDummyTodo(todo) {
+        assert todo.getId() == "101"
+        assert todo.getUser() == "someone"
+        assert todo.getTask() == "walk the dog"
+        assert todo.isCompleted() == false
+    }
+    
 }
